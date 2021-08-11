cd34cab3ba67764d8de7ac9282113d5121191ac7
Angelibz001 <86499361+Angelibz001@users.noreply.github.com>
2021-08-11 01:42:28 -0500

    Create syntax-of-testjs-file.js

---
+++ syntax-of-testjs-file.js
@@ -0,0 +1,15 @@
+const nameYouWantToGive = artifacts.requier("nameofcontractinsolidity");
+
+
+contract("name of contract",()=>{
+
+
+beforeEach(async ()=>{
+
+});
+
+it("what should happened", async ()=>{
+
+});
+
+});
